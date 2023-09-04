3e2d3594f31a86c66c534861b17556136136881a
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-04 20:26:02 +0200

    Modify mocks and add limit on get method

--- src/mocks/mocksRackets.ts
+++ src/mocks/mocksRackets.ts
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
-import { type RacketStructure } from "../database/type";
-
-const mockId = new mongoose.Types.ObjectId().toString();
+import { type RacketStructure } from "../server/type.js";
 
 export const mockRackets: RacketStructure[] = [
   {
@@ -16,9 +14,11 @@ export const mockRackets: RacketStructure[] = [
       "Its low balance also makes it very easy to use, and probably part of the reason Momo has been able to pull off so many amazing “tweeners”.",
     image:
       "https://global-uploads.webflow.com/63b3fa45463266b20f659562/649e6f862b0a67ce08e19fb2_64956aba227b3b839fb4ec8c_6470cbdbd3b6b46a21ebf792_Puma%252520Solar%252520Attack%252520Momo%252520Review.png",
+    favorite: false,
+    user: new mongoose.Schema.Types.ObjectId("User"),
   },
   {
-    _id: mockId,
+    _id: new mongoose.Types.ObjectId().toString(),
     name: "Adidas Metalbone 3.2",
     shape: "Diamond shape",
     weight: 355,
@@ -29,5 +29,7 @@ export const mockRackets: RacketStructure[] = [
       "Slightly softer core, slightly increasing sweet spot size and core reactivity. Intermediate-advanced level players looking for power and control.",
     image:
       "https://global-uploads.webflow.com/63b3fa45463266b20f659562/649e6f8305268fb9a97f602b_64217f65b74db7945744bd8f_641980999afb8b103ce7dc93_Metalbone%2525203.2%252520Review.png",
+    favorite: false,
+    user: new mongoose.Schema.Types.ObjectId("User"),
   },
 ];
