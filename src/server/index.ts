2fc638675ab39913d4715da26fa8fe5b0e1d0207
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-04 15:49:16 +0200

    Fix some bugs on cors and space lines

--- src/server/index.ts
+++ src/server/index.ts
@@ -11,21 +11,22 @@ import { pingController } from "./controllers/pingController/pingController.js";
 
 const corsOptions = {
   origin: [process.env.ALLOW_ORIGIN_PROD!, process.env.ALLOW_ORIGIN_LOCAL!],
-  methods: "GET, POST, DELETE, PATCH",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
 };
 
 const app = express();
+
 app.disable("x-powered-by");
 
 app.use(cors(corsOptions));
+
 app.use(morgan("dev"));
+
 app.use(express.json());
 
 app.get("/", pingController);
 
 app.use(generalErrorHandler);
+
 app.use(endpointNotFound);
 
 export default app;
