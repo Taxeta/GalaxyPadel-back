ac1b64d691714e2d457bd716841613504fe07288
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-06 11:17:03 +0200

    Install firebase library and configuration

---
+++ src/server/firebase.ts
@@ -0,0 +1,12 @@
+import "dotenv/config.js";
+import admin from "firebase-admin";
+
+const firebaseApp = admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  }),
+});
+
+export default firebaseApp;
