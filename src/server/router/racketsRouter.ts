18a882eca193e7e26e829d382e0ebb3bf199786f
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-09-16 18:18:26 +0200

    Create getRacketById controller

--- src/server/router/racketsRouter.ts
+++ src/server/router/racketsRouter.ts
@@ -3,6 +3,7 @@ import {
   addRacket,
   deleteRacket,
   getRackets,
+  getRacketsById,
 } from "../controllers/racketsController/racketsControllers.js";
 import paths from "../paths/paths.js";
 
@@ -11,5 +12,6 @@ const racketsRouter = express.Router();
 racketsRouter.get(paths.rootPath, getRackets);
 racketsRouter.delete(paths.deleteIdPath, deleteRacket);
 racketsRouter.post(paths.rootPath, addRacket);
+racketsRouter.get(paths.racketByIdPath, getRacketsById);
 
 export default racketsRouter;
